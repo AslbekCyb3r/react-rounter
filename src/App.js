64747ff7864fda31a1a12e4b61c755d1d5f05383
aslbek <rizoxonrizoxon903@gmail.com>
2026-05-08 12:19:08 +0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/home';
-import About from './pages/abount';
+import Users from './pages/users';
+import About from './pages/Todos';
 import Product from './pages/produsk';
 import Header from './component/Header';
 import Concat from './pages/concat';
@@ -12,7 +12,7 @@ function App() {
     <div className="App bg-sky-200 h-300">
 
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Users />} />
         <Route path='/about' element={<About />} />
         <Route path='/product' element={<Product />} />
         <Route path='/concat' element={<Concat />} />
